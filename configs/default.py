7176e76e5d7bdb70d5331c9230a9b546d678bb8c
Rasmus Kard <rasmuskard@gmail.com>
2026-08-14 12:51:44 +0300

    change: change default config

--- configs/default.py
+++ configs/default.py
@@ -19,33 +19,30 @@ import sqlalchemy.types as dtype
 
 config_dict = {
     "tables": {
-        "bruhtable": {
-            "dtypes": {
-                # dtype key should match a `column_name`
-                # dtypes can be empty but then whatever default is set in `globals` is used for SQL dtypes
-            },
-            "values": {
-                # `imdb_dataset_value_column` : `column_name`
-                "tconst": "movieID",
-                "titleType": "titleType",
-                "runtimeMinutes": "runtimeMinutes",
-                "numVotes": "numVotes",
-            },
-        },
         "title": {
-            "dtypes": {"movieID": dtype.String(20)},
-            "values": {
-                "tconst": "movieID",
-                "genres": "Genres",
-            },
+        "values": {
+            "tconst": "tconst",
+            "titleType": "titleType",
+            "primaryTitle": "primaryTitle",
+            "startYear": "startYear",
+            "runtimeMinutes": "runtimeMinutes",
+            "averageRating": "averageRating",
+            "numVotes": "numVotes"
+        }
         },
+        "title_genres": {
+        "values": {
+            "tconst": "tconst",
+            "genres": "genres"
+        }
+        }
     },
     "settings": {
-        "blocked_genres": {"Horror", "Musical", "Short"},
+        # "blocked_genres": {"Horror", "Musical", "Short"},
         "blocked_titletypes": {"tvEpisode", "videoGame", "tvShort"},
         "columns_to_drop": {"isAdult", "endYear", "originalTitle"},
         "is_split_genres_into_reftable": True,  # split the comma separated genres string and convert it to an int with a lookup table
-        "is_convert_title_type_str_to_int": False,  # convert to int for lookup table creation
+        "is_convert_title_type_str_to_int": True,  # convert to int for lookup table creation
         "is_remove_adult": True,
         "is_streaming": True,
         "is_ignore_db_has_tables_error": True,
